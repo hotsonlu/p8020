4ef135375038a36b5cd085acc6a0960282beaf4e
Hotson lu <learning2010@gmail.com>
2016-12-24 11:58:46 +0800

    wechath auth

---
+++ app/controllers/wechat_supports_controller.rb
@@ -0,0 +1,14 @@
+require 'digest/sha1’
+
+class WechatSupportsController < ApplicationController
+  @@token = 'shekou'
+
+  def auth_wechat
+    signature = params[:signature]
+    timestamp = params[:timestamp]
+    nonce     = params[:nonce]
+    echostr   = params[:echostr]
+    encrypt_str = Digest::SHA1.hexdigest([timestamp, nonce, TOKEN].sort.join)
+    render text: "Forbidden", status: 403  unless encrypt_str == signature
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
 
 Rails.application.routes.draw do
-
+  get '/test/wechat' => 'wechat_supports#auth_wechat'
   # writer your routes here
 
   mount Sidekiq::Web => '/sidekiq'
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
 
   get '/auth/:provider/callback' => 'sessions#create'
+
 end
