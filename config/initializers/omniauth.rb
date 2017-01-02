0c8bcd9cc80a9bfa68b042ec110cab48e59587d5
yafeilee <lyfi2003@gmail.com>
2017-01-02 21:57:46 +0800

    Trying adjust omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :wechat, Figaro.env.OMNIAUTH_WECHAT_KEY, Figaro.env.OMNIAUTH_WECHAT_SECRET,
-    provider_ignores_state: true
+  provider :wechat, Figaro.env.OMNIAUTH_WECHAT_KEY, Figaro.env.OMNIAUTH_WECHAT_SECRET, auth_token_params: {}, provider_ignores_state: true
 end
